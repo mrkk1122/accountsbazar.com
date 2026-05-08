911a91bb80ae0cab96450fcb833e0a50334b1247
Khokon_Mondol <khokonkhokon7990@gmail.com>
2026-05-08 00:27:42 -0500

    Delete robots directory

--- robots/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-logout();
-header('Location: login.php');
-exit;
--- robots/admin/reports.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-requireAdminLogin();
-header('Location: orders.php');
-exit;
